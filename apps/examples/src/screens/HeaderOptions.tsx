56a59a989d48820e1155c53aeb49c9088600d94a
Janic Duplessis <janicduplessis@gmail.com>
2024-07-05 02:33:53 -0400

    Update example

--- apps/examples/src/screens/HeaderOptions.tsx
+++ apps/examples/src/screens/HeaderOptions.tsx
@@ -1,19 +1,11 @@
-import React, { useState, useLayoutEffect, useEffect } from 'react';
-import { StyleSheet, ScrollView, Text, Platform } from 'react-native';
 import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
-import {
-  SettingsInput,
-  SettingsPicker,
-  SettingsSwitch,
-  Square,
-  Button,
-  ToastProvider,
-  useToast,
-} from '../shared';
+import { Button, SettingsSwitch, Square, ToastProvider } from '../shared';
 
 type StackParamList = {
   Main: undefined;
@@ -40,133 +32,43 @@ const MainScreen = ({ navigation }: MainScreenProps): React.JSX.Element => {
   );
 };
 
-type HeaderItemPosition = 'left' | 'center' | 'right';
-type HeaderTitleAlignment = 'left' | 'center';
-
 interface SettingsScreenProps {
   navigation: NativeStackNavigationProp<StackParamList, 'Settings'>;
 }
 
-const SettingsScreen = ({ navigation }: SettingsScreenProps): React.JSX.Element => {
-  const toast = useToast();
-
-  const [headerTitle, setHeaderTitle] = useState('Settings');
-  const [backButtonVisible, setBackButtonVisible] = useState(true);
-  const [headerShown, setHeaderShown] = useState(true);
-  const [headerTitleAlign, setHeaderTitleAlign] =
-    useState<HeaderTitleAlignment>('left');
-  const [headerLargeTitle, setHeaderLargeTitle] = useState(true);
-  const [headerItem, setHeaderItem] = useState<HeaderItemPosition>('right');
-  const [headerBackTitle, setHeaderBackTitle] = useState('Back');
-  const [headerShadowVisible, setHeaderShadowVisible] = useState(false);
-  const [headerTransparent, setHeaderTransparent] = useState(false);
-
-  const square = (props: { tintColor?: string }) => (
-    <Square {...props} color="green" size={20} />
-  );
+const SettingsScreen = ({
+  navigation,
+}: SettingsScreenProps): React.JSX.Element => {
+  const [headerLargeItem, setHeaderLargeItem] = useState(false);
 
   useLayoutEffect(() => {
+    const square1 = (props: { tintColor?: string }) => (
+      <View style={{ gap: 8, flexDirection: 'row' }}>
+        {headerLargeItem && <Square {...props} color="green" size={20} />}
+        <Square {...props} color="green" size={20} />
+      </View>
+    );
+
+    const square2 = (props: { tintColor?: string }) => (
+      <Square {...props} color="red" size={20} />
+    );
+
     navigation.setOptions({
-      headerBackVisible: backButtonVisible,
-      headerBackTitleVisible: backButtonVisible, // iOS
-      headerLargeTitle, // iOS
-      headerBackTitle, // iOS
-      headerShown,
-      headerTitleAlign, // Android
-      headerRight: headerItem === 'right' ? square : undefined,
-      headerTitle: headerItem === 'center' ? square : headerTitle,
-      headerLeft: headerItem === 'left' ? square : undefined,
-      headerShadowVisible,
-      headerTransparent,
+      headerRight: square1,
+      headerTitle: undefined,
+      headerLeft: headerLargeItem ? square2 : undefined,
+      headerBackTitleVisible: false,
     });
-  }, [
-    navigation,
-    headerTitle,
-    backButtonVisible,
-    headerLargeTitle,
-    headerBackTitle,
-    headerItem,
-    headerTitleAlign,
-    headerShown,
-    headerShadowVisible,
-    headerTransparent,
-  ]);
+  }, [navigation, headerLargeItem]);
 
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"
       style={styles.container}>
-      <SettingsInput
-        label="Header title"
-        value={headerTitle}
-        onValueChange={setHeaderTitle}
-      />
       <SettingsSwitch
-        label="Back button visible"
-        value={backButtonVisible}
-        onValueChange={setBackButtonVisible}
-      />
-      <SettingsSwitch
-        label="Header shown"
-        value={headerShown}
-        onValueChange={setHeaderShown}
-      />
-      <SettingsPicker<HeaderTitleAlignment>
-        label="Header title align"
-        value={headerTitleAlign}
-        onValueChange={setHeaderTitleAlign}
-        items={['left', 'center']}
-      />
-      <SettingsSwitch
-        label="Header shadow visible"
-        value={headerShadowVisible}
-        onValueChange={setHeaderShadowVisible}
-      />
-      <SettingsSwitch
-        label="Header transparent"
-        value={headerTransparent}
-        onValueChange={setHeaderTransparent}
-      />
-      <SettingsPicker<HeaderItemPosition>
-        label="Header item"
-        value={headerItem}
-        onValueChange={item => {
-          if (item === 'left' && backButtonVisible) {
-            // to make header's item ideally on the left side,
-            // we need to hide the back button.
-            toast.push({
-              message: 'Hiding back button...',
-              backgroundColor: 'orange',
-            });
-            setBackButtonVisible(false);
-          }
-          if (
-            item === 'center' &&
-            Platform.OS === 'android' &&
-            headerTitleAlign !== 'center'
-          ) {
-            // on Android, we can't have a header item in the center
-            // and a title at the same time
-            toast.push({
-              message: 'Changing title alignment to center...',
-              backgroundColor: 'orange',
-            });
-            setHeaderTitleAlign('center');
-          }
-          setHeaderItem(item);
-        }}
-        items={['left', 'center', 'right']}
-      />
-      <Text style={styles.heading}>iOS only</Text>
-      <SettingsSwitch
-        label="Header large title"
-        value={headerLargeTitle}
-        onValueChange={setHeaderLargeTitle}
-      />
-      <SettingsInput
-        label="Header back title"
-        value={headerBackTitle}
-        onValueChange={setHeaderBackTitle}
+        label="Header large item"
+        value={headerLargeItem}
+        onValueChange={setHeaderLargeItem}
       />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </ScrollView>
@@ -177,10 +79,7 @@ const Stack = createNativeStackNavigator<StackParamList>();
 
 const App = (): React.JSX.Element => (
   <ToastProvider>
-    <Stack.Navigator
-      screenOptions={{
-        headerBackVisible: false,
-      }}>
+    <Stack.Navigator>
       <Stack.Screen
         name="Main"
         options={{
